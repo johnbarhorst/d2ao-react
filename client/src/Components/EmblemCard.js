2f6b1122120aec47ac062a37a1f9dad3736d6a18
John Barhorst <crashdsinc@comcast.net>
2020-01-07 23:13:30 -0600

    Emblem card work

--- client/src/Components/EmblemCard.js
+++ client/src/Components/EmblemCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const EmblemCard = (props) => {
-  const Card = styled.div`
-  background-image: url(${props => 'http://www.bungie.net' + props.bgPath});
-  width: 300px;
-  height: 100px;
+const Card = styled.div`
+  background-image: url(${props => 'https://www.bungie.net' + props.bgPath});
+  color: rgb(221, 201, 24);
+  width: 474px;
+  height: 96px;
 `;
+
+const classTypeRef = ["Titan", "Hunter", "Warlock"];
+const genderTypeRef = ["Male", "Female"];
+const raceTypeRef = ["Human", "Awoken", "Exo"];
+
+
+const EmblemCard = (props) => {
 
   return (
     <Card bgPath={props.emblemBackgroundPath}>
       <p>{props.light}</p>
+      <p>{genderTypeRef[props.genderType]}</p>
+      <p>{raceTypeRef[props.raceType]}</p>
+      <p>{classTypeRef[props.classType]}</p>
     </Card>
   )
 }
