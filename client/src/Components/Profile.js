b02c326f5be35c2ff6cfba41e0d8e7a09b555c5e
John Barhorst <crashdsinc@comcast.net>
2020-01-29 16:22:47 -0600

    some light styling started

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -2,9 +2,16 @@ import React, { useContext } from 'react';
 import { Route, Switch, Link, useRouteMatch } from 'react-router-dom';
 import { UserContext } from '../Contexts';
 import { useSetPageTitle } from '../Hooks';
+import styled from 'styled-components';
 import EmblemCard from './EmblemCard';
 import Guardian from './Guardian';
 
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+  margin: 0 10px;
+`;
 
 const Profile = () => {
   const UserInfo = useContext(UserContext);
@@ -18,22 +25,26 @@ const Profile = () => {
         <div>
           <h3>{userProfile.username}</h3>
         </div>
-        {guardians.map(guardian =>
-          <Link to={{
-            pathname: `${url}/${guardian.characterId}`,
-            state: { guardian }
-          }}
-            key={guardian.characterId}>
-            <EmblemCard {...guardian} />
-          </Link>
-        )}
-        <div>
-          <Switch>
-            <Route path={`${path}/:guardianId`}
-              render={value => <Guardian guardianInfo={value.location.state.guardian} />}
-            />
-          </Switch>
-        </div>
+        <Wrapper>
+          <div>
+            {guardians.map(guardian =>
+              <Link to={{
+                pathname: `${url}/${guardian.characterId}`,
+                state: { guardian }
+              }}
+                key={guardian.characterId}>
+                <EmblemCard {...guardian} />
+              </Link>
+            )}
+          </div>
+          <div>
+            <Switch>
+              <Route path={`${path}/:guardianId`}
+                render={value => <Guardian guardianInfo={value.location.state.guardian} />}
+              />
+            </Switch>
+          </div>
+        </Wrapper>
         <div>
         </div>
       </>
